cefd0b464e3480a10f1cba110c444afb2fafe9b4
Nalbert, Vinicius <rullaxfox@gmail.com>
2022-04-24 12:01:30 -0300

    configurações inicias da base de dados

---
+++ APICatalogo/Context/APICatalogoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APICatalogo.Models;
+
+namespace APICatalogo.Context
+{
+    public class APICatalogoContext : DbContext
+    {
+        //Config do contexto
+        public APICatalogoContext(DbContextOptions<APICatalogoContext> options) : base(options)
+        {
+
+        }
+
+        //Set das tabelas
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Categoria> Produtos { get; set; }
+
+    }
+}
--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,3 +1,6 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<APICatalogoContext>(options => 
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
 
 var app = builder.Build();
 
